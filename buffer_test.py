5c6f0c9471836c4a20c6219dba4b7c60ff1fb772
Ctacus <ctacus@gmail.com>
2026-01-13 01:05:08 +0400

    trim start of video test

--- buffer_test.py
+++ buffer_test.py
@@ -16,6 +16,13 @@ class MyTestCase(unittest.TestCase):
 
         self.assertListEqual(windows, list(buffer.window_totals))
 
+    def test_trim(self):
+        buffer = ClipBuffer(1, 4)
+        motion = [0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0]
+        windows = [3, 2, 3, 2, 1, 1, 0]
+        for m in motion:
+            buffer.append(np.empty(0), m)
+        self.assertListEqual(windows, list(buffer.window_totals))
 
 if __name__ == '__main__':
     unittest.main()
